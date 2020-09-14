91adfec2dd1c8193ca42bb102fd1d99b1a64624f
Fa2y <a.merzoukbenselloua@esi-sba.dz>
2020-09-14 23:12:44 +0000

    fixing configurations for production

--- onlinelearn/classroom/urls.py
+++ onlinelearn/classroom/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from classroom import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 
 urlpatterns = [
 	url(r'^signup/$', views.SignupView, name='signup' ),
@@ -23,5 +23,9 @@ urlpatterns = [
 	url(r'^quizz/taken/(?P<pk>[0-9]+)$',views.TakenQuizzView, name='result-quizz'),
 	url(r'^quizz/list/$',views.ListQuizzView, name='list-quizz'),
 	url(r'^$', views.HomeView, name='home'),
+	url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+	url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 #.....setting up static route like this just for debugging 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
--- onlinelearn/onlinelearn/settings.py
+++ onlinelearn/onlinelearn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n*sqow+&kyzcsnzu1=63!=!tk$xl4o*3_a8ok5pc@_2+(#v*)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
